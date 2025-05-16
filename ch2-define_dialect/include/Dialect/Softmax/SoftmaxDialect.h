067018d793c0fde27ca43d875614ec5cdb8668ae
shaokai.wang <shaokai.wang@terapines.com>
2025-05-16 16:00:06 +0800

    [softmax][ch2]: Modify CMake file 配置好ch2的CMakeLists.txt文件使其,能正确编译

--- ch2-define_dialect/include/Dialect/Softmax/SoftmaxDialect.h
+++ ch2-define_dialect/include/Dialect/Softmax/SoftmaxDialect.h
@@ -1,6 +1,9 @@
 #ifndef __SOFTMAX_DIALECT_H__
 #define __SOFTMAX_DIALECT_H__
 
+#include <mlir/IR/Dialect.h>
+#include <mlir/IR/MLIRContext.h>
 
+#include "Dialect/Softmax/SoftmaxDialect.h.inc"
 
 #endif // __SOFTMAX_DIALECT_H__
